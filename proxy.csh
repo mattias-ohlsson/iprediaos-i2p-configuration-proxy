5732c2ab2eda1fea1be90d021f75f1f5d8588359
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-04-05 17:35:49 +0200

    Update proxy settings in profile.d scripts

--- proxy.csh
+++ proxy.csh
@@ -1,2 +1,4 @@
 setenv http_proxy http://localhost:4444
 setenv HTTP_PROXY http://localhost:4444
+setenv https_proxy https://localhost:4445
+setenv HTTPS_PROXY https://localhost:4445
--- proxy.sh
+++ proxy.sh
@@ -1,2 +1,4 @@
 export http_proxy=http://localhost:4444
 export HTTP_PROXY=http://localhost:4444
+export https_proxy=https://localhost:4445
+export HTTPS_PROXY=https://localhost:4445
