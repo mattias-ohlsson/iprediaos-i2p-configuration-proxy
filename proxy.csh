7a052664c0e3a36d331b9b8500ee2730b2b55e43
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-04-10 14:42:12 +0200

    Add no_proxy

--- proxy.csh
+++ proxy.csh
@@ -1,3 +1,5 @@
+setenv no_proxy localhost,127.0.0.0/8,*.local
+setenv NO_PROXY localhost,127.0.0.0/8,*.local
 setenv http_proxy http://localhost:4444
 setenv HTTP_PROXY http://localhost:4444
 setenv https_proxy https://localhost:4445
--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,5 @@
+export no_proxy=localhost,127.0.0.0/8,*.local
+export NO_PROXY=localhost,127.0.0.0/8,*.local
 export http_proxy=http://localhost:4444
 export HTTP_PROXY=http://localhost:4444
 export https_proxy=https://localhost:4445
